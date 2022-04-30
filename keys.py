8318db52a2166708df8e015145a13754c9feb20a
Suvid Datta <suvid.datta@gmail.com>
2022-04-30 12:10:05 +0530

    HMMM

---
+++ keys.py
@@ -0,0 +1,10 @@
+username = "KJW6k1f7cA8yQ7xpssU8UaxnK04V4lUC8ypdXet2"
+ip = "192.168.1.2"
+CLIENTSECRET = "b88572709bd045d2814484c0409a3074"
+CLIENTID = "9f1af6e0ec314efdb650596aa1c7ce46"
+SCOPE = "user-read-playback-state user-modify-playback-state user-read-currently-playing user-library-modify " \
+        "user-library-read streaming app-remote-control user-read-playback-position user-top-read " \
+        "user-read-recently-played playlist-modify-private playlist-read-collaborative playlist-read-private " \
+        "playlist-modify-public"
+TOKEN = "AQDXY5A23JT67CqefgNBAwPPJyqR78V_Iwl2ZhMiyI2ZUrzfPHbGXZBDxeglNct5QMaK9XmxWRgeGGr0Aki-dGPpbj2SCVXNuZEvQ6TAp46JXnDQOtakI6BEQJoEWG5ioaSh30p_NrG_WP9nPcqZUqQKvDSIxt2Q4UYd_3hsRIrxH0tDGtU3EaU_-bzghIQZI18BzQYGnypa8uQR0Y3JJKQO1iNDBAmoyS_oEMG1sHQnlksr2t-c45RMoQuLyZi409YEpnQbsFFjVnAZl2R_0IkheOwdElDHEHQ5payU8_MG36_8OB5HKa-iAH8xpvk6_FdiaQDREak3ALOUxOLrsFEUrcsZHm4vE62AiyGD4Hgibm3KFpvCIaGa4pJkaw2eJs4P7znRDENbZnVgK8JtcmxLef77BQyVVXzXsnmStICWAgoVUEur6KaCxIxwdJh5WJ9CdH4BYSMskLVEY7vYs74ilrIl_qb3w2fWjfRXdsAPznccW3tCq5OC_r10SVoSFS2RYxj9V4rJsQuqgfOzBpBRSNnKZB0EfoVNbLlzQ1EyypQys3yeonQe2awKRiy_TYWalD05ugopxFm2EqN6PFuHSw"
+SIU = "http://www.amazeryogo.in/"
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, redirect, url_for
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy
+import pprint
+import flask_bootstrap
+from keys import *
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENTID,
+                                               client_secret=CLIENTSECRET,
+                                               redirect_uri=SIU,
+                                               scope=SCOPE))
+app = Flask(__name__)
+flask_bootstrap.Bootstrap(app)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@app.route('/')
+def index():
+    # get the song that the user is listening to
+    current_song = sp.current_user_playing_track()
+    playlists = []
+    #get user id
+    user = sp.current_user()
+    #get user's playlists
+    x = sp.user_playlists(user['id'])
+    for i in x['items']:
+        playlists.append(i)
+    #get user's top tracks
+    top_tracks = sp.current_user_top_tracks(limit=10, time_range='short_term')
+    #get user's top artists
+    pprint.pprint(current_song)
+    return render_template('index.html', playlists=playlists, top_tracks=top_tracks,current_song=current_song)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.run(debug=True)
